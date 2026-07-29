d0f4a847f34a46cdbc764ca50c0069e4fc2be9ed
wizzygrace247 <wisemma2006@gmail.com>
2026-07-29 19:57:41 +0100

    Allow Vercel domain in CORS

--- Devotify-backend/main.py
+++ Devotify-backend/main.py
@@ -37,7 +37,7 @@ app = FastAPI(title="Devotify Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://https://devotify.vercel.app",],
+                   "https://devotify.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
